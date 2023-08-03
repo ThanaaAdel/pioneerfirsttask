c5ad0e70ecd9e73d93493e1032fe67799003a8c1
ThanaaAdel <swaadl971@gmail.com>
2023-08-03 19:12:47 +0300

    commit in modify in order by date2

--- lib/Features/order_date/domain/repository/order_date_repo.dart
+++
@@ -1,6 +0,0 @@
-import 'package:dartz/dartz.dart';
-import 'package:pioneerfirsttask/core/errors/failure.dart';
-import '../../../repository_home/domain/entities/repository_entity.dart';
-abstract class OrderDateRepo {
-  Future<Either<Failure, List<RepositoryEntity>>> fetchRepositoryByDate();
-}
